02f39a3407421ba75328ebe4e970967aa4b338cc
EmungZakaria <56185837+EmungZakaria@users.noreply.github.com>
2020-10-16 00:40:17 +0700

    Create listAnime.js

---
+++ listAnime.js
@@ -0,0 +1,13 @@
+export default [{
+    animeEmoji: '💪👊👨‍🦳😻',
+    animeName: '3-gatsu no Lion',
+    animeYear: 2016,
+    animeLink: 'https://myanimelist.net/anime/31646/3-gatsu_no_Lion',
+  },
+  {
+    animeEmoji: '🗼 🔮 💥 🏍️',
+    animeName: 'Akira',
+    animeYear: 1988,
+    animeLink: 'https://myanimelist.net/anime/47/Akira',
+  },
+];  
